217d45cc7542c1110888f5567def40b4820b9cfd
BorutD <borut.drazumeric@gmail.com>
2018-01-30 23:20:00 +0200

    Added clickable elements.

--- Tic-Tac-Toe/src/app.js
+++ Tic-Tac-Toe/src/app.js
@@ -6,36 +6,30 @@ var TicTacToeGame = (function () {
             height: 0
         };
         this.canvas = document.getElementById('canvas');
+        this.canvas.addEventListener('click', this.handleClick);
         this.ctx = this.canvas.getContext('2d');
         this.game.width = document.getElementById('canvas').width;
         this.game.height = document.getElementById('canvas').height;
         this.ctx.fillStyle = "#D6E5FF";
         this.ctx.fillRect(0, 0, this.game.width, this.game.height);
-        // this.drawGame(this.game.width / 2, this.game.height / 2, 200, 200, { x: 0.5, y: 0.5 });
-        // this.drawGameLayout();
-        this.drawGameLayout2(300, 100, 400);
+        this.drawGameLayout();
+        // this.drawGameLayout2(this.game.width / 2, this.game.height / 2, 400, { x: 0.5, y: 0.5 });
     }
     TicTacToeGame.prototype.drawGameLayout2 = function (x, y, width, anchor) {
-        var square = this.canvas.getContext("2d");
-        square.fillStyle = "#FF0000";
-        square.fillRect(x, y, width, width);
+        if (anchor) {
+            var x = x - (width * anchor.x);
+            var y = y - (width * anchor.y);
+        }
         this.drawLine(width * (2 / 3) + x, y, width * (2 / 3) + x, width + y);
         this.drawLine(width * (1 / 3) + x, y, width * (1 / 3) + x, width + y);
         this.drawLine(x, width * (1 / 3) + y, width + x, width * (1 / 3) + y);
         this.drawLine(x, width * (2 / 3) + y, width + x, width * (2 / 3) + y);
     };
-    TicTacToeGame.prototype.drawGame = function (x, y, width, height, anchor) {
-        var square = this.canvas.getContext("2d");
-        square.fillStyle = "#FF0000";
-        var x = x - (width * anchor.x);
-        var y = y - (height * anchor.y);
-        square.fillRect(x, y, width, height);
-    };
     TicTacToeGame.prototype.drawGameLayout = function () {
-        this.drawLine(150, 20, 150, 420);
-        this.drawLine(280, 20, 280, 420);
-        this.drawLine(20, 150, 420, 150);
-        this.drawLine(20, 280, 420, 280);
+        this.drawLine(150, 20, 150, 420, 5);
+        this.drawLine(280, 20, 280, 420, 5);
+        this.drawLine(20, 150, 420, 150, 5);
+        this.drawLine(20, 280, 420, 280, 5);
         for (var i = 0; i < 3; i++) {
             for (var j = 0; j < 3; j++) {
                 var x = 24 + (j * 130);
@@ -46,13 +40,25 @@ var TicTacToeGame = (function () {
             }
         }
     };
-    TicTacToeGame.prototype.drawLine = function (x1, x2, y1, y2) {
+    TicTacToeGame.prototype.handleClick = function (e) {
+        var position = this.getMousePosition(e);
+        console.log(position.x, position.y);
+    };
+    TicTacToeGame.prototype.getMousePosition = function (event) {
+        var rect = this.canvas.getBoundingClientRect();
+        return {
+            x: event.clientX - rect.left,
+            y: event.clientY - rect.top,
+        };
+    };
+    TicTacToeGame.prototype.drawLine = function (x1, x2, y1, y2, lineWidth) {
+        if (lineWidth === void 0) { lineWidth = 5; }
         var line = this.canvas.getContext('2d');
         line.beginPath();
         line.moveTo(x1, x2);
         line.lineTo(y1, y2);
-        line.lineWidth = 5;
-        // line.lineCap = 'round';
+        line.lineWidth = lineWidth;
+        line.lineCap = 'round';
         line.stroke();
     };
     return TicTacToeGame;
--- Tic-Tac-Toe/src/app.ts
+++ Tic-Tac-Toe/src/app.ts
@@ -9,6 +9,7 @@ class TicTacToeGame {
 
 	constructor() {
 		this.canvas = document.getElementById('canvas');
+		this.canvas.addEventListener('click', this.handleClick);
 		this.ctx = this.canvas.getContext('2d');
 
 		this.game.width = (document as any).getElementById('canvas').width;
@@ -17,15 +18,15 @@ class TicTacToeGame {
 		this.ctx.fillStyle = "#D6E5FF";
 		this.ctx.fillRect(0, 0, this.game.width, this.game.height);
 
-		// this.drawGame(this.game.width / 2, this.game.height / 2, 200, 200, { x: 0.5, y: 0.5 });
-		// this.drawGameLayout();
-		this.drawGameLayout2(300, 100, 400);
+		this.drawGameLayout();
+		// this.drawGameLayout2(this.game.width / 2, this.game.height / 2, 400, { x: 0.5, y: 0.5 });
 	}
 
 	private drawGameLayout2(x: number, y: number, width: number, anchor?: Anchor) {
-		var square = this.canvas.getContext("2d");
-		square.fillStyle = "#FF0000";
-		square.fillRect(x, y, width, width);
+		if (anchor) {
+			var x = x - (width * anchor.x);
+			var y = y - (width * anchor.y);
+		}
 
 		this.drawLine(width * (2 / 3) + x, y, width * (2 / 3) + x, width + y);
 		this.drawLine(width * (1 / 3) + x, y, width * (1 / 3) + x, width + y);
@@ -34,21 +35,12 @@ class TicTacToeGame {
 		this.drawLine(x, width * (2 / 3) + y, width + x, width * (2 / 3) + y);
 	}
 
-	private drawGame(x: number, y: number, width: number, height: number, anchor: Anchor) {
-		var square = this.canvas.getContext("2d");
-		square.fillStyle = "#FF0000";
-		var x = x - (width * anchor.x);
-		var y = y - (height * anchor.y);
-		square.fillRect(x, y, width, height);
-	}
-
 	private drawGameLayout() {
-		this.drawLine(150, 20, 150, 420);
-		this.drawLine(280, 20, 280, 420);
+		this.drawLine(150, 20, 150, 420, 5);
+		this.drawLine(280, 20, 280, 420, 5);
 
-		this.drawLine(20, 150, 420, 150);
-		this.drawLine(20, 280, 420, 280);
-
+		this.drawLine(20, 150, 420, 150, 5);
+		this.drawLine(20, 280, 420, 280, 5);
 
 		for (var i = 0; i < 3; i++) {
 			for (var j = 0; j < 3; j++) {
@@ -61,13 +53,28 @@ class TicTacToeGame {
 		}
 	}
 
-	private drawLine(x1: number, x2: number, y1: number, y2: number): void {
+	private handleClick(e) {
+		var position = this.getMousePosition(e);
+
+		console.log(position.x, position.y);
+	}
+
+	private getMousePosition(event: any): Object {
+		var rect = this.canvas.getBoundingClientRect();
+
+		return {
+			x: event.clientX - rect.left,
+			y: event.clientY - rect.top,
+		}
+	}
+
+	private drawLine(x1: number, x2: number, y1: number, y2: number, lineWidth: number = 5): void {
 		var line = this.canvas.getContext('2d');
 		line.beginPath();
 		line.moveTo(x1, x2);
 		line.lineTo(y1, y2);
-		line.lineWidth = 5;
-		// line.lineCap = 'round';
+		line.lineWidth = lineWidth;
+		line.lineCap = 'round';
 		line.stroke();
 	}
 }
